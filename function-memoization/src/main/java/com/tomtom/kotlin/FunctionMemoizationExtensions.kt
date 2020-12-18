29275a0cc2263bc97d8b6e2e6d085995b8a5d8e6
Krzysztof Karczewski <krzysztof.karczewski@tomtom.com>
2020-12-18 17:41:56 +0100

    Update copyright header. Add idea config.

--- function-memoization/src/main/java/com/tomtom/kotlin/FunctionMemoizationExtensions.kt
+++ function-memoization/src/main/java/com/tomtom/kotlin/FunctionMemoizationExtensions.kt
@@ -1,6 +1,5 @@
 /*
- * Copyright 2020-present, TomTom N.V.
- *
+ * Copyright (C) 2020-2020, TomTom (http://tomtom.com).
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
