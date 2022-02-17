cc18db68dfc55641ee5b5dec8bfc69cc3adaa34b
tvkanters <tvkanters@gmail.com>
2022-02-17 20:00:38 +0100

    Cleanup based on PR comments

--- extensions/src/main/java/com/tomtom/kotlin/extensions/CollectionExtensions.kt
+++ extensions/src/main/java/com/tomtom/kotlin/extensions/CollectionExtensions.kt
@@ -19,7 +19,10 @@ package com.tomtom.kotlin.extensions
 /**
  * Checks whether the collection contains duplicated elements.
  */
-fun Collection<*>.containsDuplicates(): Boolean = toSet().size != size
+fun Iterable<*>.containsDuplicates(): Boolean {
+    val elements = mutableSetOf<Any?>()
+    return any { !elements.add(it) }
+}
 
 /**
  * Checks whether the collection contains no duplicated elements.
